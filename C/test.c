c57dca2aa1367b0180172f8bb2ab4848d3ad9324
Maksym Vyshnyakov <maksymvyshnyakov@Maksyms-MacBook-Pro.local>
2025-09-28 20:41:45 +0200

    readTSP working, tested

---
+++ C/test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "data.h"
+#include "data.c"
+
+
+int main(int argc, char* argv[]) {
+    
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    char path[256];
+    snprintf(path, sizeof(path), "../JDD/ALL_tsp/%s.tsp", argv[1]);
+
+    FischierTSP* f;
+    readTSP(path, f);
+    if (f->edge_type==ATT){
+        printf("ATT\n");
+        for (int i = 0; i< f->dimension; i++){
+        printf("%d %d %d\n", f->nodes[i].numero, f->nodes[i].coord.att.x, f->nodes[i].coord.att.y);
+        }
+    }
+    else if (f->edge_type==EUC_2D){
+        printf("EUC_2D\n");
+        for (int i = 0; i< f->dimension; i++){
+        printf("%d %f %f\n", f->nodes[i].numero, f->nodes[i].coord.euc.x, f->nodes[i].coord.euc.y);
+        }
+    }
+    else if (f->edge_type==GEO){
+        printf("GEO\n");
+        for (int i = 0; i< f->dimension; i++){
+        printf("%d %f %f\n", f->nodes[i].numero, f->nodes[i].coord.geo.lt, f->nodes[i].coord.geo.lgt);
+        }
+    }
+    
+    printf("fin\n");
+    return 0;
+}
