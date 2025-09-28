f1e844df9f8a87079eefc5052dc2406c0a32c77d
Maksym Vyshnyakov <maksymvyshnyakov@Maksyms-MacBook-Pro.local>
2025-09-28 21:21:11 +0200

    corrected small mistake

--- C/testTSP.c
+++ C/testTSP.c
@@ -8,31 +8,33 @@
 int main(int argc, char* argv[]) {
     
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+       tspprintf(stderr, "Usage: %s <filename>\n", argv[0]);
         return 1;
     }
 
     char path[256];
-    snprintf(path, sizeof(path), "../JDD/ALL_tsp/%s", argv[1]);
+    snprintf(path, sizeof(path), "../JDD/ALL_tsp/%s.tsp", argv[1]);
 
-    FischierTSP* f;
-    readTSP(path, f);
-    if (f->edge_type==ATT){
+    FischierTSP* tsp;
+    readTSP(path,tsp);
+    printf("Tour name: %s\n",tsp->name);
+    printf("Dimension: %d\n",tsp->dimension);
+    if (tsp->edge_type==ATT){
         printf("EDGE_WEIGHT_TYPE: ATT\n");
-        for (int i = 0; i< f->dimension; i++){
-        printf("%d %d %d\n", f->nodes[i].numero, f->nodes[i].coord.att.x, f->nodes[i].coord.att.y);
+        for (int i = 0; i<tsp->dimension; i++){
+        printf("%d %d %d\n",tsp->nodes[i].numero,tsp->nodes[i].coord.att.x,tsp->nodes[i].coord.att.y);
         }
     }
-    else if (f->edge_type==EUC_2D){
+    else if (tsp->edge_type==EUC_2D){
         printf("EDGE_WEIGHT_TYPE: EUC_2D\n");
-        for (int i = 0; i< f->dimension; i++){
-        printf("%d %f %f\n", f->nodes[i].numero, f->nodes[i].coord.euc.x, f->nodes[i].coord.euc.y);
+        for (int i = 0; i<tsp->dimension; i++){
+        printf("%d %f %f\n",tsp->nodes[i].numero,tsp->nodes[i].coord.euc.x,tsp->nodes[i].coord.euc.y);
         }
     }
-    else if (f->edge_type==GEO){
+    else if (tsp->edge_type==GEO){
         printf("EDGE_WEIGHT_TYPE: GEO\n");
-        for (int i = 0; i< f->dimension; i++){
-        printf("%d %f %f\n", f->nodes[i].numero, f->nodes[i].coord.geo.lt, f->nodes[i].coord.geo.lgt);
+        for (int i = 0; i<tsp->dimension; i++){
+        printf("%d %f %f\n",tsp->nodes[i].numero,tsp->nodes[i].coord.geo.lt,tsp->nodes[i].coord.geo.lgt);
         }
     }
     
