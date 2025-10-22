1211d26ceb396b134078465be00408872653c2ab
Maksym Vyshnyakov <maksymvyshnyakov@Maksyms-MacBook-Pro.local>
2025-10-22 16:31:14 +0200

    main works for test (except longueur)

--- C/main.c
+++ C/main.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <time.h>
 #include "data.h"
 #include "fonctions_calcul.h"
 #include "force_brute.h"
 #include "Demi matrice.h"
 
 int main(int argc, char* argv[]){
+    clock_t start, end;
+    
     if(argc!=2){
         printf("Usage: %s [nom_fichier_tsp]\n", argv[0]);
         exit(0);
@@ -19,15 +22,30 @@ int main(int argc, char* argv[]){
 
     FichierTSP* tsp = malloc(sizeof(FichierTSP));
     readTSP(path,tsp);
-    //printf("Instance ; algo ; long (C) ; long (Python) ; temps ; tour ; valid ; mêmes longueurs");
     char *instance = tsp->name;
-    char *algo = "cannonical";
-    long long_C;
-    long long_Python = 0;
-    int temps;
-    // tour;
-    int valid = 0;
-    int memes_longueurs = (long_C==long_Python);
-    force_brute(tsp, tsp->edge_type);
+    char *methode = "cannonical";
+    double temps_cpu;
+    int longueur;
+
+    
+    start = clock();    
+    FichierTour* tour = force_brute(tsp, tsp->edge_type);
+    end = clock();
+    
+
+    temps_cpu = ((double)(end - start)) / CLOCKS_PER_SEC;   
+    
+    longueur = longueur_tournee(tsp, tour, tsp->edge_type);
+    printf("Instance ; Méthode ; Temps CPU (sec) ; Longueur ; Tour\n");
+    printf("%s.tsp ; %s ; %lf ; %d ; [", argv[1], methode, temps_cpu, longueur);
+
+    for (int i = 0; i<tour->dimension;i++){
+        if (i==(tour->dimension-1)){
+            printf("%d]\n", tour->nodes[i]);
+            break;
+        }
+        printf("%d, ", tour->nodes[i]);
+    }
+
     return 0;
 }
